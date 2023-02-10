723c67c91321cda5c9e7f565d6605de4c52bdce8
Saurav8005 <sauravchauhan8005@gmail.com>
2023-02-11 00:19:05 +0530

    backend code

---
+++ index.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+const port = process.env.PORT || 5000
+const API = process.env.DATABASE_URL || "mongodb+srv://Sagarika:Sagarika@cluster0.u0ncdyq.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.set('strictQuery', false);
+
+const app = require('./app');
+dotenv.config();
+
+async function main() {
+    await mongoose.connect(API);
+    console.log('connected to database');
+    app.listen(port, () => console.log(`Server is live at PORT => ${port}`));
+};
+main();
